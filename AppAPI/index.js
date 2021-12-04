1f62ad8a99e2367b52e3481a9517d5646304b1c5
Jacob <vb2553yq@go.minnstate.edu>
2021-12-04 10:46:26 -0600

    First push of final project

---
+++ AppAPI/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const nodemon = require('nodemon');
+app.use(express.json());
+
+//MongoDB Package
+const mongoose = require('mongoose');
+
+const PORT = 1250;
+
+const dbUrl = "mongodb+srv://admin:Passw0rd@cluster0.vpu9l.mongodb.net/G2Final?retryWrites=true&w=majority";
+
+//Connect to MongoDB
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//MongoDB Connection
+const db = mongoose.connection;
+
+//Handle DB Error, display connection
+db.on('error', () => {
+    console.error.bind(console,'connection error: ');
+});
+db.once('open',() => {
+    console.log('MongoDB Connected')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Started on port ${PORT}`);
+})
