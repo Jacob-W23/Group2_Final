3693fdb224ca6fe182cb1eabce2f5497849cf812
Abdullahi <tb2332wz@go.minnstate.edu>
2021-12-12 13:26:02 -0600

    fixed the issue with the null error

--- lib/movies.dart
+++ lib/movies.dart
@@ -4,20 +4,19 @@ import 'main.dart';
 import 'AppAPI.dart';
 
 class Movies extends StatefulWidget {
-  String id, moviesPoster, movieName, movieDescription;
-  Movies(this.id, this.moviesPoster, this.movieName, this.movieDescription);
+  String moviesPoster, movieName, movieDescription;
+  Movies(this.moviesPoster, this.movieName, this.movieDescription);
 
   final AppAPI api = new AppAPI();
 
   @override
   _MoviesState createState() =>
-      _MoviesState(id, moviesPoster, movieName, movieDescription);
+      _MoviesState(moviesPoster, movieName, movieDescription);
 }
 
 class _MoviesState extends State<Movies> {
-  String id, moviePoster, movieName, movieDescription;
-  _MoviesState(
-      this.id, this.moviePoster, this.movieName, this.movieDescription);
+  String moviePoster, movieName, movieDescription;
+  _MoviesState(this.moviePoster, this.movieName, this.movieDescription);
   bool loaded = false;
   List movies = [];
 
@@ -39,63 +38,70 @@ class _MoviesState extends State<Movies> {
         title: Text("Movies List"),
       ),
       body: loaded
-          ? ListView(
+          ? Column(
               children: [
-                ...movies
-                    .map<Widget>(
-                      (movieParam) => Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 0.0),
-                        child: TextButton(
-                          onPressed: () => {
-                            Navigator.pop(context),
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Movies(
-                                          movieParam['id'],
-                                          movieParam['moviePoster'],
-                                          movieParam['movieName'],
-                                          movieParam['movieDescription'],
-                                        ))),
-                          },
-                          child: Container(
-                            padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                            margin: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              border:
-                                  Border.all(width: 1, color: Colors.purple),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(8),
-                              ),
-                            ),
-                            child: ListTile(
-                              leading: Image.network(
-                                movieParam['moviePoster'],
-                                width: 120.0,
-                                height: 600.0,
-                                fit: BoxFit.fill,
-                              ),
-                              title: Text(
-                                (movieParam['movieName']),
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              subtitle: Text(movieParam['movieDescription']),
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => ShowMovies(
-                                        movieParam['moviePoster'],
-                                        movieParam['movieName'],
-                                        movieParam['movieDescription']),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ...movies
+                          .map<Widget>(
+                            (movieParam) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 0.0),
+                              child: TextButton(
+                                onPressed: () => {
+                                  //Navigator.pop(context),
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Movies(
+                                                movieParam['moviePoster'],
+                                                movieParam['movieName'],
+                                                movieParam['movieDescription'],
+                                              ))),
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                  margin: EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        width: 1, color: Colors.purple),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(8),
+                                    ),
                                   ),
-                                );
-                              },
+                                  child: ListTile(
+                                    leading: Image.network(
+                                      movieParam['moviePoster'],
+                                      width: 120.0,
+                                      height: 600.0,
+                                      fit: BoxFit.fill,
+                                    ),
+                                    title: Text(
+                                      (movieParam['movieName']),
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                    subtitle:
+                                        Text(movieParam['movieDescription']),
+                                    onTap: () {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) => ShowMovies(
+                                              movieParam['moviePoster'],
+                                              movieParam['movieName'],
+                                              movieParam['movieDescription']),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                      ),
-                    )
-                    .toList(),
+                          )
+                          .toList(),
+                    ],
+                  ),
+                ),
               ],
             )
           : Center(
